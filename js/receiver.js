ce6d554d21bf6a92dda516ee2919abfe3daee428
Dharaneesh Venkatesh <dharaneesh.venkatesh@axonista.com>
2021-06-24 14:51:07 +0100

    Debugger disabled from script

--- js/receiver.js
+++ js/receiver.js
@@ -27,10 +27,10 @@ const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
 
 // Enable debug logger and show a warning on receiver
 // NOTE: make sure it is disabled on production
-castDebugLogger.setEnabled(true);
+castDebugLogger.setEnabled(false);
 
 // Show debug overlay
-// castDebugLogger.showDebugLogs(true);
+castDebugLogger.showDebugLogs(false);
 
 playerManager.addEventListener(
     cast.framework.events.category.CORE,
