da9f7609f539412df418ac0fd20dabc9c2f7d817
Sergey Mingalev <sergey.mingalev@axonista.com>
2022-09-27 12:17:20 +0700

    adsloader added

--- js/receiver.js
+++ js/receiver.js
@@ -4,6 +4,17 @@ const mediaElement = document.getElementsByTagName("cast-media-player")[0].getMe
 const streamManager = new google.ima.cast.dai.api.StreamManager(mediaElement);
 const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
 
+let adsLoader;
+let adDisplayContainer;
+let adsManager;
+
+// Preroll ad tag
+const TEST_AD_TAG = 'https://pubads.g.doubleclick.net/gampad/ads?' +
+    'iu=/21775744923/external/single_ad_samples&sz=640x480&' +
+    'cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&' +
+    'output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=';
+
+
 const getStreamRequest = (request) => {
   const imaRequestData = request.media.customData;
   let streamRequest = null;
@@ -27,21 +38,39 @@ playerManager.setMessageInterceptor(
 
         request.media.hlsSegmentFormat = cast.framework.messages.HlsSegmentFormat.TS;
 
-        if (request.media.customData && request.media.customData.adTagUrl) {
-            request.media.vmapAdsRequest = {
-                adTagUrl: request.media.customData.adTagUrl
-            };
-            return request;    
-        } else {
-          return streamManager.requestStream(request, getStreamRequest(request))
-          .then((request) => {
-            return Promise.resolve(request);
-            })
-          .catch((error) => {
-            return Promise.resolve(request);
-            });
-        }
+        // if (request.media.customData && request.media.customData.adTagUrl) {
+        //     request.media.vmapAdsRequest = {
+        //         adTagUrl: request.media.customData.adTagUrl
+        //     };
+        //     return request;    
+        // } else {
+          // return streamManager.requestStream(request, getStreamRequest(request))
+          // .then((request) => {
+          //   return Promise.resolve(request);
+          //   })
+          // .catch((error) => {
+          //   return Promise.resolve(request);
+          //   });
+          requestAdddd("123")
+//        }
   });
+
+
+  function requestAdddd(qqq) {
+    // Client side ads setup.
+  adDisplayContainer = new google.ima.AdDisplayContainer(mediaElement);
+  // Must be done as the result of a user action on mobile
+  adDisplayContainer.initialize();
+
+  adsLoader = new google.ima.AdsLoader(adDisplayContainer);
+  adsLoader.addEventListener(
+    google.ima.AdsManagerLoadedEvent.Type.ADS_MANAGER_LOADED,
+    onAdsManagerLoaded, false);
+  adsLoader.addEventListener(
+    google.ima.AdErrorEvent.Type.AD_ERROR, onAdError, false);
+
+  requestPreroll(TEST_AD_TAG);
+  }
 
 /** LOAD interceptor **/
 /**
@@ -89,28 +118,53 @@ playerManager.addEventListener(
 
 context.start();
 
+/**
+ * Requests a preroll ad using the client side SDK.
+ * @param {string} adTagUrl
+ */
+ function requestPreroll(adTagUrl) {
+  const adsRequest = new google.ima.AdsRequest();
+  adsRequest.adTagUrl = adTagUrl;
+  adsRequest.linearAdSlotWidth = 640;
+  adsRequest.linearAdSlotHeight = 400;
+  adsLoader.requestAds(adsRequest);
+}
 
 /**
- * Shows the video controls so users can resume after stream is paused.
+ * Handles the adsManagerLoaded event (client side ads).
+ * @param {!google.ima.dai.api.AdsManagerLoadedEvent} adsManagerLoadedEvent
  */
- function onStreamPause() {
-  console.log('paused');
-  if (isAdBreak) {
-    videoElement.controls = true;
-    adUiDiv.style.display = 'none';
+ function onAdsManagerLoaded(adsManagerLoadedEvent) {
+  adsManager = adsManagerLoadedEvent.getAdsManager(mediaElement);
+  adsManager.addEventListener(google.ima.AdErrorEvent.Type.AD_ERROR, onAdError);
+  adsManager.addEventListener(
+      google.ima.AdEvent.Type.CONTENT_PAUSE_REQUESTED, function(e) {
+        console.log('Content pause requested.');
+      });
+  adsManager.addEventListener(
+      google.ima.AdEvent.Type.CONTENT_RESUME_REQUESTED, function(e) {
+        console.log('Content resume requested.');
+        requestLiveStream(TEST_ASSET_KEY, null);
+      });
+  try {
+    adsManager.init(640, 360, google.ima.ViewMode.NORMAL);
+    adsManager.start();
+  } catch (adError) {
+    // An error may be thrown if there was a problem with the VAST response.
   }
 }
 
 /**
- * Hides the video controls if resumed during an ad break.
+ * Handles an ad error (client side ads).
+ * @param {!google.ima.dai.api.AdErrorEvent} adErrorEvent
  */
-function onStreamPlay() {
-  console.log('played');
-  if (isAdBreak) {
-    videoElement.controls = false;
-    adUiDiv.style.display = 'block';
+ function onAdError(adErrorEvent) {
+  console.log(adErrorEvent.getError());
+  if (adsManager) {
+    adsManager.destroy();
   }
 }
+
 
 /**
 if (context.start() != null) {
